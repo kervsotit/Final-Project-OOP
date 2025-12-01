ee368f94503de19a7125bb9f8acaa33f845736a2
celissyril0-tec <celissyril0@gmail.com>
2025-12-01 10:41:29 +0800

    Add files via upload

---
+++ BillsExpense.java
@@ -0,0 +1,6 @@
+public class BillsExpense extends Expenses {
+    public BillsExpense(String date, String description, double amount) {
+        super(date, "Bills", description, amount);
+    }
+}
+
---
+++ TransportExpense.java
@@ -0,0 +1,5 @@
+public class TransportExpense extends Expenses {
+    public TransportExpense(String date, String description, double amount) {
+        super(date, "Transport", description, amount);
+    }
+}
