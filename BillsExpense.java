4282954f5f56620d1c91ba39b4e4cc386c2ba25a
celissyril0-tec <celissyril0@gmail.com>
2025-12-01 10:44:57 +0800

    Delete BillsExpense.java

--- BillsExpense.java
+++
@@ -1,6 +0,0 @@
-public class BillsExpense extends Expenses {
-    public BillsExpense(String date, String description, double amount) {
-        super(date, "Bills", description, amount);
-    }
-}
-
