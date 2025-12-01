75d1891c4e76cd0856762e200af1acfe1c2946e1
celissyril0-tec <celissyril0@gmail.com>
2025-12-01 10:43:42 +0800

    Delete TransportExpense.java

--- TransportExpense.java
+++
@@ -1,5 +0,0 @@
-public class TransportExpense extends Expenses {
-    public TransportExpense(String date, String description, double amount) {
-        super(date, "Transport", description, amount);
-    }
-}
